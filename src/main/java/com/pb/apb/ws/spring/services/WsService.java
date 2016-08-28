e58441dcd8361522402aff8a63c75679aec20a0f
Alex <Alex@Alex-PC>
2016-08-28 11:17:18 +0300

    Убрал неиспользуемый импорт

--- src/main/java/com/pb/apb/ws/spring/services/WsService.java
+++ src/main/java/com/pb/apb/ws/spring/services/WsService.java
@@ -6,9 +6,7 @@
 package com.pb.apb.ws.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
